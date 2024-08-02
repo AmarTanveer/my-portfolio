c77f029c31fc9f888c3575e7db330502c5d97fb6
Amar Tanveer <amartanveer@gmail.com>
2024-08-02 21:20:39 +0530

    Added logo in the sidebar

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Sidebar.css";
 import "../index.css";
 import Content from "../content/Content";
+import logo from "../../public/logoportfolio.svg";
 
 function Sidebar() {
   function scrollTo(sectionId) {
@@ -9,9 +10,10 @@ function Sidebar() {
   }
   return (
     <div className="Sidebar">
-      <p onClick={() => scrollTo("intro")} style={{cursor: "pointer"}}>
-        A<span style={{ color: "var(--blue)" }}>.</span>
-      </p>
+      <div className="logo" onClick={() => scrollTo("intro")} style={{cursor: "pointer"}}>
+      
+        <img src={logo} alt="" style={{width: "65%", objectFit: "cover"}}/>
+      </div>
 
       <h3 onClick={() => scrollTo("about")}  style={{cursor: "pointer"}}>About</h3>
       <h3 onClick={() => scrollTo("projects")}  style={{cursor: "pointer"}}>Projects</h3>
