8e23a3601050d4452e636962873007c3ec289266
Amar Tanveer <amartanveer@gmail.com>
2025-04-23 14:03:57 +0530

    Fixed non-working project links

--- src/content/Content.jsx
+++ src/content/Content.jsx
@@ -782,7 +782,7 @@ function Content() {
                 old aged users. Added interactive prototyping, and engaging
                 animations.{" "}
                 <a
-                  href="https://shorturl.at/VK8NK"
+                  href="https://www.figma.com/proto/NAhuZ1RRE2eHBL5wHPEvr4/Ritch-Website?page-id=0%3A1&node-id=1-2&starting-point-node-id=1%3A2&t=jSyU2r4hQNfuvvEs-1"
                   target="_blank"
                   style={{
                     textDecoration: "none",
